91b6d4673fccf0cd4ed2abf6915137d2570d0bd5
SSK137 <saurabhkodam@gmail.com>
2022-05-31 23:33:50 +0530

    Added Welcome Message

--- src/main/java/com/bridgelabz/EmployeePayrollMain.java
+++ src/main/java/com/bridgelabz/EmployeePayrollMain.java
@@ -2,6 +2,6 @@ package com.bridgelabz;
 
 public class EmployeePayrollMain {
     public static void main(String[] args) {
-        System.out.println("");
+        System.out.println("$$$--Welcome To JDBC--$$$");
     }
 }
