3315d11d2e7cd57e1abb6081d3b11bd8c510e89d
SSK137 <saurabhkodam@gmail.com>
2022-05-31 23:58:56 +0530

    Added sql query to create new database

---
+++ src/test/resources/EmployeePayrollJDBC.sql
@@ -0,0 +1,34 @@
+======================UC1==============================
+//Using MySQL Client use create database
+  query to create a payroll_service
+  database
+mysql> create database employee_payroll;
+Query OK, 1 row affected (0.02 sec)
+
+//Also you can see the DB created by
+  using show database query
+mysql> show databases;
++--------------------+
+| Database           |
++--------------------+
+| AddressBook        |
+| AddressBookService |
+| Address_Book_DB    |
+| Employee_Payroll   |
+| Payroll_Service    |
+| Sample             |
+| employee_payroll   |
+| information_schema |
+| mysql              |
+| performance_schema |
+| sys                |
++--------------------+
+11 rows in set (0.01 sec)
+
+//And go to the database created by using
+  use payroll_service query
+mysql> use employee_payroll;
+Database changed
+
+======================UC2==============================
+
